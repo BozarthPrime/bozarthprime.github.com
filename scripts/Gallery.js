d50507e90c01e897e14b90c76a41def8431b4845
Joseph Bozarth <bozarjp@gmail.com>
2014-10-13 21:51:44 -0500

    Gallery
    Made the gallery not horrible

--- scripts/Gallery.js
+++ scripts/Gallery.js
@@ -4,16 +4,39 @@
             height: 200,
             width: 100,
 			heightIsPercent: false,
-			widthIsPercent: true
+			widthIsPercent: true,
+			nextImage: "",
+			prevImage: "",
+			closeImage: ""
         }, options);
 
         var element = $(this);
+		var curImage;
+		var curGallery;
 		
-		$('body,html').append("<div id='shadow'></div>");
-		$('body,html').append("<div id='lightbox'></div>");
+		$('body').append("<div id='shadow'></div>");
+		$('body').append("<div id='lightbox'></div>");
 		$('#lightbox').append("<div id='img-cont'></div>");
 		$('#lightbox').append("<div id='img-note'></div>");
-		$('#lightbox').append("<div id='close-button' title='Close'>Close</div>");
+		
+		if (settings.closeImage != "") {
+			$('#lightbox').append("<div id='img-close'><img src='" + settings.closeImage + "' /></div>");
+		} else {
+			$('#lightbox').append("<div id='img-close'>X</div>");
+		}
+		
+		if (settings.nextImage != "") {
+			$('#lightbox').append("<div id='img-next'><img src='" + settings.nextImage + "' /></div>");
+		} else {
+			$('#lightbox').append("<div id='img-next'>Next</div>");
+		}
+		
+		if (settings.prevImage != "") {
+			$('#lightbox').append("<div id='img-prev'><img src='" + settings.prevImage + "' /></div>");
+		} else {
+			$('#lightbox').append("<div id='img-prev'>Prev.</div>");
+		}
+		
 		
 		element.css('text-align', 'center');
 		element.css('vertical-align', 'middle');
@@ -38,43 +61,102 @@
 		galleryImages.css('display', 'inline-block');
 		
 		function close() {
-			$('#lightbox').hide(function() {
+			$('#lightbox').hide(0,function() {
+				$('#img-note').hide();
 				$('#shadow').slideUp(function(){
 					$('#img-cont').html('');
 					$('#img-note').html('');
 				});
 			});
+			
+			curImage = null;
+		}
+		
+		function nextImage() {
+			if (curImage < curGallery.length - 1) {
+				curImage++;
+			} else {
+				curImage = 0;
+			}
+			
+			setImage();
+		}
+		
+		function prevImage() {
+			if (curImage > 0) {
+				curImage--;
+			} else {
+				curImage = curGallery.length - 1;
+			}
+			
+			setImage();
+		}
+		
+		function setImage() {
+			var image = $(curGallery[curImage]).clone();
+
+			if(image.attr('comment')) {
+				$('#img-note').html(image.attr('comment'));
+				$('#img-note').show();
+			} else {
+				$('#img-note').hide();
+			}
+
+			image.css('max-height', '100%');
+			image.css('max-width', '100%');
+			image.css('display', 'block');
+			
+			$('#img-cont').html(image);			
 		}
 		
 		galleryImages.click(function() {
-			$('#shadow').slideDown();
-			
+			curGallery = $(this).parent().children('img');
 			var image = $(this).clone();
 			
-			image.css('max-height', '85%');
+			for(var i = 0; i < curGallery.length; i++) {
+				if ($(curGallery[i]).attr('src') == $(this).attr('src')) {
+					curImage = i;
+				}
+			}
+			
+			$('#shadow').slideDown(function(){
+				if(image.attr('comment')) {
+					$('#img-note').html(image.attr('comment'));
+					$('#img-note').show();
+				} else {
+					$('#img-note').hide();
+				}
+			});
+
+			image.css('max-height', '100%');
 			image.css('max-width', '100%');
 			image.css('display', 'block');
 			
 			image.hide();
 			$('#img-cont').html(image);
-			
-			if($(this).attr('comment')) {
-				$('#img-note').html($(this).attr('comment'));
-				$('#img-note').show();
-			} else {
-				$('#img-note').hide();
-			}
-			
+
 			$('#lightbox').show(function() {
-				image.slideDown(250);
+				image.slideDown(200);
 			});
 		});
 		
-		$('#close-button').click(function() {
-			close();
+		$('#img-cont').click(function() {
+			nextImage();
+		});
+		
+		$('#img-next').click(function() {
+			nextImage();
+		});
+		
+		$('#img-prev').click(function() {
+			prevImage();
 		});
 		
 		$('#shadow').click(function() {
+			close();
+		});
+		
+		$('#img-close').click(function() {
 			close();
 		});
 		
